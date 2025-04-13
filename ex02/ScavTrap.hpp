6591dfd260723d29c2de11165c467dd6c72296ee
chhoflac <chhoflac@student.42.fr>
2025-04-13 17:00:40 +0200

    finishing scavtrap

--- ex02/ScavTrap.hpp
+++ ex02/ScavTrap.hpp
@@ -1,17 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/10 11:19:05 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/04/13 16:56:53 by chhoflac         ###   ########.fr       */
+/*   Created: 2025/04/13 15:27:13 by chhoflac          #+#    #+#             */
+/*   Updated: 2025/04/13 16:09:15 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
 
-int main(void){
+class ScavTrap : public ClapTrap {
+	public :
+		ScavTrap();
+		~ScavTrap();
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap &src);
+		ScavTrap &operator = (const ScavTrap &otherOne);
+		
+		void	guardGate();
+		void	attack(const std::string &target);
+};
 
-}
+#endif

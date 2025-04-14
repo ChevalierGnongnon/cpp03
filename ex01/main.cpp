432addb5bbc226fdce160526a76b935b9247a9f0
chhoflac <chhoflac@student.42.fr>
2025-04-14 20:10:59 +0200

    small mods

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 11:19:05 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/04/14 17:44:35 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/04/14 20:09:09 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,33 @@
 
 int main(void){
 	ScavTrap trappix("a");
-	ScavTrap trappax();
-	std::cout << trappix.name 
+	ScavTrap trappax;
+	
+
+	std::cout << "<-------------members function called------------->" << std::endl;
+	std::cout << "name: " << trappix.getName() << ", HP : " << trappix.getHitPoints() << " damage: " << trappix.getAttackDamage() << " EP: " << trappix.getEnergyPoints() << std::endl;
+	std::cout << "name: " << trappax.getName() << ", HP : " << trappax.getHitPoints() << " damage: " << trappax.getAttackDamage() << " EP: " << trappax.getEnergyPoints() << std::endl;
+	trappix.attack("trappax");
+	trappax.attack("other one");
+	trappax.takeDamage(10);
+	trappax.beRepaired(2);
+	trappax.guardGate();
+	
+	std::cout << "<-------------tests copy constructor------------->" << std::endl;
+	ScavTrap copyTrap("copy");
+	std::cout << "Name: " << copyTrap.getName() << ", HP: " << copyTrap.getHitPoints() << ", EP: " << copyTrap.getEnergyPoints() << ", Damage: " << copyTrap.getAttackDamage() << std::endl;
+	copyTrap.attack("other one");
+	copyTrap.takeDamage(10);
+	copyTrap.beRepaired(2);
+	copyTrap.guardGate();
+	
+	std::cout << "<-------------copy assignment operator test------------->" << std::endl;
+	ScavTrap assignedTrap;
+	
+    assignedTrap = copyTrap;
+    std::cout << "Name: " << assignedTrap.getName() << ", HP: " << assignedTrap.getHitPoints() << ", EP: " << assignedTrap.getEnergyPoints() << ", Damage: " << assignedTrap.getAttackDamage() << std::endl;
+	assignedTrap.attack("other one");
+	assignedTrap.takeDamage(10);
+	assignedTrap.beRepaired(2);
+	assignedTrap.guardGate();
 }
